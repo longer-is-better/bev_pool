a73d60ccce150aa5c78c905f0c9db350237be7dd
Wu Yong <wuyong5@lixiang.com>
2023-12-06 10:30:10 +0800

    add new type

--- bev_pool.py
+++ bev_pool.py
@@ -422,6 +422,30 @@ def test_v2_half_half_float():
     print(f"v2: half_half_float: {(t1-t0)*1000:.3f} ms")
     compare_tensors(bev_local, bev_baseline_float)
 
+def test_v2_half_float_float():
+    depth_local = depth.half().cuda()
+    feat_local = feat.cuda()
+    bev_local = bev.cuda()
+    ranks_depth_local = ranks_depth.cuda()
+    ranks_feat_local = ranks_feat.cuda()
+    interval_starts_local = interval_starts_e.cuda()
+    interval_lengths_local = interval_lengths_e.cuda()
+
+    t0 = time.time()
+    EXE.bev_pool_v2_half_float_float(ctypes.c_int(C),
+                                    ctypes.c_int(n_intervals),
+                                    ctypes.c_void_p(depth_local.data_ptr()),
+                                    ctypes.c_void_p(feat_local.data_ptr()),
+                                    ctypes.c_void_p(ranks_depth_local.data_ptr()),
+                                    ctypes.c_void_p(ranks_feat_local.data_ptr()),
+                                    ctypes.c_void_p(interval_starts_local.data_ptr()),
+                                    ctypes.c_void_p(interval_lengths_local.data_ptr()),
+                                    ctypes.c_void_p(bev_local.data_ptr()))
+    torch.cuda.synchronize()
+    t1 = time.time()
+    print(f"v2: half_float_float: {(t1-t0)*1000:.3f} ms")
+    compare_tensors(bev_local, bev_baseline_float)
+
 
 
 n_interval = 50000
