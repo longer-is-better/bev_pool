232fdfb0d20069dbebd05f76b2739ba15720a152
Wu Yong <wuyong5@lixiang.com>
2023-12-12 02:00:15 +0800

    add flatmap_sorted

--- bev_pool.py
+++ bev_pool.py
@@ -37,7 +37,6 @@ interval_lengths_x = torch.zeros((3000000), dtype=torch.int32)
 
 
 
-
 n_intervals = OH * OW
 n_intervals_x = ctypes.c_int(0)
 ranks_bev_mask = torch.zeros(OH * OW, dtype=torch.int8)
@@ -64,6 +63,29 @@ EXE.tensor_NDHW_to_NHWD(ctypes.c_void_p(ranks_depth_nhwd.data_ptr()),
                         ctypes.c_int(IW))
 
 feat_nchw = torch.permute(feat, (0, 3, 1, 2)).contiguous()
+
+def trim_neg1(t):
+    # Find the index of the first occurrence of -1 in the tensor
+    last_non_neg_index = (t != -1).nonzero(as_tuple=False).max()
+    # Slice the tensor up to the last non-negative index to remove the trailing -1 values
+    trim = t[:last_non_neg_index + 1]
+    return trim
+
+def sort_abc_by_a(a, b, c):
+    abc = torch.stack([a, b, c])
+    # Sort the tensor along the first dimension (axis 0) based on the values in the first 256 numbers
+    sorted_indices = torch.argsort(abc[0])
+    sorted_abc = abc[:, sorted_indices]
+
+    # Split the sorted tensor into a1, b1, and c1 with 256 elements each
+    a1 = sorted_abc[0]
+    b1 = sorted_abc[1]
+    c1 = sorted_abc[2]
+    return a1, b1, c1
+
+ranks_bev_sorted, ranks_depth_sorted, ranks_feat_sorted = sort_abc_by_a(trim_neg1(ranks_bev),
+                                                                        trim_neg1(ranks_depth),
+                                                                        trim_neg1(ranks_feat))
 
 def compare_tensors(tensor1, tensor2, rtol=1e-03, atol=1e-05):
     closeness = torch.isclose(tensor1, tensor2, atol=atol, rtol=rtol)
@@ -110,6 +132,33 @@ def test_flatmap():
     t1 = time.time()
     print(f"flatmap: {(t1-t0)*1000:.3f} ms")
     compare_tensors(bev_local, bev_baseline_float)
+
+def test_flatmap_sorted():
+    depth_local = depth.cuda()
+    feat_local = feat.cuda()
+    bev_local = bev.cuda()
+    ranks_depth_local = ranks_depth_sorted.cuda()
+    ranks_feat_local = ranks_feat_sorted.cuda()
+    ranks_bev_local = ranks_bev_sorted.cuda()
+    interval_starts_local = interval_starts.cuda()
+    interval_lengths_local = interval_lengths.cuda()
+
+    t0 = time.time()
+    EXE.bev_pool_flatmap(ctypes.c_int(C),
+                         ctypes.c_int(n_intervals),
+                         ctypes.c_void_p(depth_local.data_ptr()),
+                         ctypes.c_void_p(feat_local.data_ptr()),
+                         ctypes.c_void_p(ranks_depth_local.data_ptr()),
+                         ctypes.c_void_p(ranks_feat_local.data_ptr()),
+                         ctypes.c_void_p(ranks_bev_local.data_ptr()),
+                         ctypes.c_void_p(interval_starts_local.data_ptr()),
+                         ctypes.c_void_p(interval_lengths_local.data_ptr()),
+                         ctypes.c_void_p(bev_local.data_ptr()))
+    torch.cuda.synchronize()
+    t1 = time.time()
+    print(f"flatmap_sorted: {(t1-t0)*1000:.3f} ms")
+    compare_tensors(bev_local, bev_baseline_float)
+
 
 def test_float_float_float():
     depth_local = depth.cuda()
@@ -588,6 +637,7 @@ if __name__ == "__main__":
     test_hpc_bev_pool_v2()
     test_hpc_bev_pool_pack32_half()
     test_flatmap()
+    test_flatmap_sorted()
 
     test_float_float_float_nhwd()
     test_float_float_float_nchw()
